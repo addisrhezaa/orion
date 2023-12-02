07a58efd0e7d67cde880782b842ae2440c5aace2
HomeItID <homeit.id@gmail.com>
2023-12-03 00:11:42 +0700

    first commit

---
+++ catalogue/app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+
+    return render(request, 'index.html')
+
+def detailproduk(request):
+
+    return render(request, 'portfolio-details.html')
